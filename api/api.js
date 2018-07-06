b806dbf036b4e3627b7292049aa8fcbe3277e2d4
Nanda Scott <nanda1123@gmail.com>
2018-07-06 13:59:12 -0400

    Shit's broke

--- api/api.js
+++ api/api.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
 const sqliteJson = require('sqlite-json');
+const _ = require('lodash');
 
 app.get('/', function (err, res) {
     res.end('<h1>Hello</h1>')
 });
 
-app.get('/readValues', function (req, res) {
-    var db = new sqlite3.Database(__dirname + '/main.db');
-    var exporter = sqliteJson(db);
+app.get('/allValues', function (req, res) {
+    let exporter = sqliteJson(new sqlite3.Database(__dirname + '/main.db'));
 
-    exporter.json(`SELECT factory_child.number_value
-    FROM factory_child, root_table
-    WHERE factory_child.parent_factory == root_table.id;`, function (err, json) {
-        res.end(json)
+    const query = `SELECT factory_child.number_value AS "value", root_table.id AS "root_id"
+                    FROM factory_child LEFT JOIN root_table
+                    WHERE factory_child.parent_factory = root_table.id;`;
+
+    exporter.json(query, function (err, json) {
+
+        let sqliteData = JSON.parse(json)
+        let payload = sqliteData.map((a) => 
+            a.value
+        );
+
+        // var payload1 = sqliteData.map(a => a.value);
+
+        res.end(JSON.stringify(payload))
     });
 });
 
