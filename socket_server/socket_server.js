05aba4537bf212f57dc5201446a602ddef888326
Nanda Scott <nanda.scott@passportinc.com>
2018-07-27 13:52:05 -0400

    Shit's broken but I'm pushing for a laptop transfer.

--- socket_server/socket_server.js
+++ socket_server/socket_server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import io from 'socket.io';
-var path = require('path');
+const path = require('path');
+const rn = require('random-number');
+const randomNames = require('random-name');
 
 const app = express();
 
@@ -10,8 +12,6 @@ const server = app.listen(8000, function () {
 
 const socketServer = io(server);
 
-var clickCount = 0;
-
 app.use(express.static(path.join(__dirname, 'html')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 
@@ -19,9 +19,22 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
-socketServer.on('connection', function (client) {
+var clickCount = 0;
+
+socketServer.on('connection', (client) => {
 	client.on('clicked', function (indicator) {
 		clickCount++; //Here is where we make our API calls based on our websocket calls
 		socketServer.emit('buttonUpdate', clickCount);
 	});
+	// let payload = [];
+	// for (let i = 0; i < rn({ min: 1, max: 15, integer: true }); i++) {
+	// 	let arr = [];
+	// 	for (let i = 0; i < rn({ min: 1, max: 15, integer: true }); i++) {
+	// 		arr.push(rn({ min: 100, max: 999, integer: true }));
+	// 	}
+		
+	// 	payload.push({ name: randomNames(), values: arr });
+	// }
+
+	// socketServer.emit('siteRefresh', payload);
 });
