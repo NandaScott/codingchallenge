3ca3ab3b436580387bc45b71f9a2d46383a8133e
Nanda Scott <nanda1123@gmail.com>
2018-08-18 14:46:28 -0400

    Eh?

--- socket_server/socket_server.js
+++ socket_server/socket_server.js
@@ -89,4 +89,4 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(PORT, () => console.log(`Listening on port ${port}`));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
